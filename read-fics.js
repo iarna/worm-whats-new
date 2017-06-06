26b05c871351f5ecd4481d49ad71d39917c61892
Rebecca Turner <me@re-becca.org>
2017-06-06 14:33:24 -0700

    funstream@2.1.0

--- read-fics.js
+++ read-fics.js
@@ -3,8 +3,7 @@ module.exports = readFics
 const fs = require('fs')
 const ndjson = require('ndjson')
 const fun = require('funstream')
-const pump = require('./pump')
 
 function readFics (filename) {
-  return fun.ify(pump(fs.createReadStream(filename), ndjson.parse()))
+  return fun(fs.createReadStream(filename)).pipe(ndjson.parse())
 }
