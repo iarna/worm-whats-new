134a0fea1ff03098977ae6b418b3fe68726a2de1
Rebecca Turner <me@re-becca.org>
2017-06-06 14:36:35 -0700

    Add a general search term

---
+++ search.js
@@ -0,0 +1,25 @@
+'use strict'
+const fs = require('fs')
+const readFics = require('./read-fics.js')
+const approx = require('approximate-number');
+readFics(`${__dirname}/Fanfic.json`)
+  .filter(fic => fic.fandom === 'Worm')
+  .filter(fic => fic.tags.some(t => /SEARCH TERM/i.test(t)))
+  .forEach(printFic)
+
+function printFic (fic) {
+  const link = fic.identifiers.replace(/^ur[li]:/,'')
+  if (!fic.meta) console.log(fic)
+  const authorurl = fic.authorurl || fic.meta.authorUrl
+  console.log(`* [${fic.title}](${link}) by [${fic.authors}](${authorurl}) (${cstr(fic.meta.chapters.length)}, ${approx(fic.words)} words): ${fic.tags.join(', ')}`)
+  if (fic.rec) console.log(`  <br>${fic.rec}`)
+}
+
+function cstr (chapters, chapterPrefix) {
+  const pre = chapterPrefix ? `${chapterPrefix} ` : ''
+  if (chapters === 1) {
+    return `${chapters} ${pre}chapter`
+  } else {
+    return `${chapters} ${pre}chapters`
+  }
+}
