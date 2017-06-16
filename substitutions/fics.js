76fb9a86da62a7576b6125dcb38464af5c181519
Rebecca Turner <me@re-becca.org>
2017-06-15 22:14:54 -0700

    Shorten our cross-linking links

--- substitutions/fics.js
+++ substitutions/fics.js
@@ -5,10 +5,10 @@ These are fics that either:
 2. Have sequals.
 */
 module.exports = {
-  "Crouching Tiger": "https://forums.spacebattles.com/threads/crouching-tiger-worm-au-alt-power.382703/",
-  "Deputy": "https://forums.spacebattles.com/threads/deputy-internship-worm-au-au.483163/",
-  "Grimm Mother": "https://forums.spacebattles.com/threads/grimm-mother-worm-rwby.486124/",
-  "Hyperdimension Taylor": "https://forums.spacebattles.com/threads/hyperdimension-taylor.423146/",
-  "Monster": "https://forums.sufficientvelocity.com/threads/monster.29235/",
-  "With Friends Like These": "https://forums.sufficientvelocity.com/threads/with-friends-like-these-altpower-taylor-worm-x-post.9324/",
+  "Crouching Tiger": "https://forums.spacebattles.com/threads/382703",
+  "Deputy": "https://forums.spacebattles.com/threads/483163",
+  "Grimm Mother": "https://forums.spacebattles.com/threads/486124",
+  "Hyperdimension Taylor": "https://forums.spacebattles.com/threads/423146",
+  "Monster": "https://forums.sufficientvelocity.com/threads/29235",
+  "With Friends Like These": "https://forums.sufficientvelocity.com/threads/9324",
 }
