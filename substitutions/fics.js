ef0a8b9cff3dbeed2300a0a30f635ba89799e80e
Rebecca Turner <me@re-becca.org>
2017-09-15 21:26:33 -0700

    substutions for 9/8

--- substitutions/fics.js
+++ substitutions/fics.js
@@ -5,6 +5,7 @@ These are fics that either:
 2. Have sequals.
 */
 module.exports = {
+  "Building Faith": "https://forums.spacebattles.com/threads/building-faith-dresden-files-si.387284/",
   "Completely Unoriginal": "https://forums.spacebattles.com/threads/526825/",
   "Crouching Tiger": "https://forums.spacebattles.com/threads/382703",
   "Deputy": "https://forums.spacebattles.com/threads/483163",
@@ -12,6 +13,8 @@ module.exports = {
   "Hyperdimension Taylor": "https://forums.spacebattles.com/threads/423146",
   "Mixed Feelings": "https://forums.spacebattles.com/threads/mixed-feelings-worm-oc.375923/",
   "Monster": "https://forums.sufficientvelocity.com/threads/29235",
+  "Nine Lives of the Calico": "https://forums.sufficientvelocity.com/threads/worm-nine-lives-of-the-calico.30603/",
   "Puella Magi Adfligo Systema": "https://forums.sufficientvelocity.com/threads/puella-magi-adfligo-systema.2538/",
+  "Tabloid": "https://archiveofourown.org/works/8603173",
   "With Friends Like These": "https://forums.sufficientvelocity.com/threads/9324",
 }
